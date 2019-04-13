e8109af3dedc28b4fb09f543e0e672e2c3b55ddc
Alkabir-Basem <basemal201@gmail.com>
2019-04-13 12:19:20 +0100

    created js file to exesse the html id elements

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-@.JS
+// store and exesses the element that have id
+var taskInput = document.getElementById("task-input");
+var addButton = document.getElementById("add-task-but");
+var todoTask = document.getElementById("todo-list");
